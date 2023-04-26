f101c688a4d538c09afecb4f0ae378f079a023c8
Emilien <emilien.ginko@gmail.com>
2023-04-26 16:29:43 +0200

    Audio in js

--- src/components/atoms/SoundIcon.jsx
+++ src/components/atoms/SoundIcon.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "../../assets/styles/components/molecules/menuIcon.scss"
+import "../../../public/In_A_World_Of_Natural_Wonder.mp3"
+
 
 const SoundIcon = () => {
     const [isSound, setIsSound] = React.useState(true)
+    const [audio, setAudio] = React.useState(new Audio('../public/In_A_World_Of_Natural_Wonder.mp3'))
 
     React.useEffect(()=>{
-        if(isSound)
-            document.querySelector("audio").volume = 0.3
-        else  
-            document.querySelector("audio").volume = 0
+        audio.addEventListener("canplaythrough", ()=>{
+            audio.play();
+            audio.volume = 0.3
+        })
+    }, [])
+    React.useEffect(()=>{
+        if(isSound){
+            audio.play();
+            audio.volume = 0.3
+        } else {
+            audio.pause()
+        }
     }, [isSound])
 
     return (
-        <button className="soundIcon" onClick={()=>{console.log("ici");setIsSound(!isSound)}}>
+        <button className="soundIcon" onClick={()=>{;setIsSound(!isSound)}}>
             { 
                 isSound ?<svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="18.5" cy="18.5" r="17.5" transform="rotate(90 18.5 18.5)" stroke="white" stroke-miterlimit="12.7455"/>
