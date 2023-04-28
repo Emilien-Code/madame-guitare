5b648a822a24f1e2da8d28b3a60a2c14aa1cac4d
Emilien <emilien.ginko@gmail.com>
2023-04-28 16:31:44 +0200

    🔧,contact form and serverless function

--- api/contact.js
+++ api/contact.js
@@ -1,49 +1,165 @@
-import nodemailer from "nodemailer"
-
-const port = 465
-const srv = "smtp.gmail.com"
-const isSecure = true
+import Mailjet from "node-mailjet";
 
 
-async function main(dest, from, message) {
-
-  let transporter = nodemailer.createTransport({
-    host: "ssl0.ovh.net",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "***", 
-      pass: "***", 
-    },
-  });
-
-  console.log("ici", dest, from, message)
-  
-  let info = await transporter.sendMail({
-    from: '"Emilien" <look@emi-studio.fr>', 
-    to: [`emilien.ginko@gmail.com`], 
-    subject: "Prise de contact", 
-    text: `L'organisme ${from} à pris contact via votre site: Les petites Histoire de Madame Guitare. ${message}. Mail de retour : ${dest}`,
-    html: `<p>L'organisme <b>${from}</b> à pris contact via votre site: Les petites Histoire de Madame Guitare.</p><br/> <br/> <p>${message}</p><br /><br /><p>Mail de retour : ${dest}</p>`,
-  });
-
-  console.log("Message sent: %s", info.messageId);
-
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+const mailjetClient = Mailjet.apiConnect(
+  'debf1669185f888320b1ca95e3de7f3e',
+  "4bb9fdd9f9c5a4ead800db81606841df",
+);
+async function send(request) {
+  return request
+  .then((result) => {
+    console.log("Result Request", result.body);
+    return "succes";
+  })
+  .catch((err) => {
+    console.log("Error status", err);
+    return "error"
+  })
 }
 
-
-export default function handler(request, response) {
+export default  async function handler(request, response) {
 
   const {mail, organisme, message} = JSON.parse(request.body)
   
   if(message && organisme && mail){
     console.log("tout est renseigné")
 
-    main(mail, organisme, message).catch(console.error);
+    const request = mailjetClient.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: "emilienvivier1@gmail.com",
+            Name: "Madame Guitare - EMI",
+          },
+          To: [
+            {
+              Email: "emilien.ginko@gmail.com",
+              Name: "vous",
+            },
+          ],
+          TemplateLanguage: true,
+          TemplateErrorReporting: {
+            Email: "look@emi-studio.fr",
+            Name: "ERREUR MAIL MADAME GUITARE",
+          },
+          Subject: "Prise de contact",
+          TextPart: `L'organisme ${organisme} à pris contact via votre site: Les petites Histoire de Madame Guitare. ${message}. Mail de retour : ${mail}`,
+          HTMLPart: `<p>L'organisme <b>${organisme}</b> à pris contact via votre site: Les petites Histoire de Madame Guitare.</p><br/> <br/> <p>${message}</p><br /><br /><p>Mail de retour : ${mail}</p>`,
+        },
+      ],
+    })
+
+    const res = await send(request)
+
+    return response.end(res);
+
+
+  } else {
+    
+    return response.end("eRreur");
+
+  }
+} 
+
+
+/**
+ * 
+ * 
+ * 
+ * // Create en headless endpoints for sending emails with maijet with the minimal following parameters:
+// - type: type of email we want to send
+// - to: email address of the recipient
+// - TemplateId: id of the template we want to use
+
+const mailjet = require("node-mailjet");
+const { MAILJET_API_KEY, MAILJET_SECRET_KEY } = process.env;
+console.log(mailjet, MAILJET_API_KEY, MAILJET_SECRET_KEY);
+const mailjetClient = mailjet.connect(MAILJET_API_KEY, MAILJET_SECRET_KEY);
+
+const sendMail = async (data) => {
+  let request = null;
+  console.log(data);
+  switch (data.type) {
+    case "track-id":
+      request = mailjetClient.post("send", { version: "v3.1" }).request({
+        Messages: [
+          {
+            From: {
+              Email: "donael@bihua.f",
+              Name: "Librairie Expérience - Bihua",
+            },
+            To: [
+              {
+                Email: data.to,
+                Name: "vous",
+              },
+            ],
+            TemplateID: 4106178,
+            TemplateLanguage: true,
+            TemplateErrorReporting: {
+              Email: "donael.walter@hotmail.com",
+              Name: "Donaël Walter",
+            },
+            Subject:
+              "Votre numero de suivie de commande du site Librairie Expérience",
+            Variables: {
+              clientName: data.to,
+              trackNumber: data.trackId,
+              trackUrl: "https://www.laposte.fr/outils/track-a-parcel",
+            },
+          },
+        ],
+      });
+
+      await request
+        .then((result) => {
+          console.log("Result Request", result.body);
+          return {
+            statusCode: 200,
+            headers: {
+              "Access-Control-Allow-Origin": "*", // Allow from anywhere
+            },
+            body: JSON.stringify(result.body),
+          };
+        })
+        .catch((err) => {
+          console.log("Error status", err);
+          return {
+            statusCode: 500,
+            headers: {
+              "Access-Control-Allow-Origin": "*", // Allow from anywhere
+            },
+            body: JSON.stringify(err),
+          };
+        });
+      break;
+
+    default:
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*", // Allow from anywhere
+        },
+        body: "Auccun form de mail",
+      };
+      break;
   }
 
+  // if not return before, it will return an error
+  return {
+    statusCode: request ? 200 : 500,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Allow from anywhere
+    },
+    body: JSON.stringify(request.body),
+  };
+};
+
+exports.handler = async (event) => {
+  const data = JSON.parse(event.body);
+
+  return sendMail(data);
+};
 
 
-  return response.end(`contact ${request.body.mail}`);
-} 
+ */
---
+++ src/components/molecules/Form.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const From = () => {
+
+    const [isSent, setIsSent] = React.useState(false)
+
+
+    const send = (e)=>{
+        e.preventDefault
+        
+        if(true){     
+
+            fetch("/api/contact", {
+            	method: "POST",
+            	body: JSON.stringify({
+            		mail: document.querySelector('input[name="mail"]').value,
+            		organisme: document.querySelector('input[name="organisme"]').value,
+            		message: document.querySelector('textarea').value,
+            	}),
+            })
+            .then(res => {
+                setIsSent(res.ok)
+            })
+            .catch(e=>{
+            	console.log("mal passé", e)
+                setIsSent(false)
+            })
+        }
+            }
+
+
+    return (
+    <>
+        {
+            isSent ? <form  className="sent">
+                <h1>Me contacter</h1>
+                <p>Votre message à bien été envoyé</p>
+                <a href="https://www.facebook.com/people/Les-petites-histoires-de-Madame-Guitare/100063654932804/" target="_blank">
+                    <svg width="41" height="40" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20.032 0C8.9794 0 0 8.9794 0 20.032C0 30.0654 7.40049 38.3689 17.0389 39.8168L18.0847 39.9751V24.1899H13.3523V20.9852H18.0847V16.7295C18.0847 14.1089 18.7135 12.3829 19.7546 11.3C20.7956 10.2171 22.3339 9.67099 24.4745 9.67099C26.1861 9.67099 26.852 9.7747 27.4853 9.85239V12.4755H25.257C23.9926 12.4755 22.9553 13.181 22.4169 14.1206C21.8784 15.0602 21.7108 16.1821 21.7108 17.3253V20.9835H27.2861L26.79 24.1882H21.7108V40L22.7441 39.8595C32.5195 38.5337 40.064 30.1635 40.064 20.032C40.064 8.9794 31.0846 0 20.032 0ZM20.032 1.82109C30.1004 1.82109 38.2429 9.96359 38.2429 20.032C38.2429 28.8914 31.9017 36.1776 23.5319 37.8179V26.0092H28.3514L29.4096 19.1624H23.5319V17.3253C23.5319 16.3806 23.7048 15.5342 23.9961 15.0258C24.2874 14.5174 24.5682 14.2966 25.257 14.2966H29.3064V8.20381L28.5168 8.0971C27.9707 8.0232 26.7238 7.8499 24.4745 7.8499C22.0132 7.8499 19.9005 8.52039 18.4421 10.0373C16.9837 11.5543 16.2636 13.8131 16.2636 16.7295V19.1641H11.5312V26.011H16.2636V37.761C8.02828 36.0165 1.82109 28.7955 1.82109 20.032C1.82109 9.96359 9.96359 1.82109 20.032 1.82109Z" fill="white"/>
+                    </svg>
+                </a>
+            </form> : <form >
+                <h1>Me contacter</h1>
+                <input name="mail" type="email" placeholder="Adresse Email" required/>
+                <input type="text" name="organisme" placeholder="Organisme" required/>
+                <textarea rows="9" name="message" placeholder="Laissez moi un message !" required/>
+                <div>
+                    <a href="https://www.facebook.com/people/Les-petites-histoires-de-Madame-Guitare/100063654932804/" target="_blank">
+                        <svg width="41" height="40" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20.032 0C8.9794 0 0 8.9794 0 20.032C0 30.0654 7.40049 38.3689 17.0389 39.8168L18.0847 39.9751V24.1899H13.3523V20.9852H18.0847V16.7295C18.0847 14.1089 18.7135 12.3829 19.7546 11.3C20.7956 10.2171 22.3339 9.67099 24.4745 9.67099C26.1861 9.67099 26.852 9.7747 27.4853 9.85239V12.4755H25.257C23.9926 12.4755 22.9553 13.181 22.4169 14.1206C21.8784 15.0602 21.7108 16.1821 21.7108 17.3253V20.9835H27.2861L26.79 24.1882H21.7108V40L22.7441 39.8595C32.5195 38.5337 40.064 30.1635 40.064 20.032C40.064 8.9794 31.0846 0 20.032 0ZM20.032 1.82109C30.1004 1.82109 38.2429 9.96359 38.2429 20.032C38.2429 28.8914 31.9017 36.1776 23.5319 37.8179V26.0092H28.3514L29.4096 19.1624H23.5319V17.3253C23.5319 16.3806 23.7048 15.5342 23.9961 15.0258C24.2874 14.5174 24.5682 14.2966 25.257 14.2966H29.3064V8.20381L28.5168 8.0971C27.9707 8.0232 26.7238 7.8499 24.4745 7.8499C22.0132 7.8499 19.9005 8.52039 18.4421 10.0373C16.9837 11.5543 16.2636 13.8131 16.2636 16.7295V19.1641H11.5312V26.011H16.2636V37.761C8.02828 36.0165 1.82109 28.7955 1.82109 20.032C1.82109 9.96359 9.96359 1.82109 20.032 1.82109Z" fill="white"/>
+                        </svg>
+                    </a>
+                    <button id="send" type="button" onClick={send}>Envoyer</button>
+                </div>
+            </form>
+        }
+    </>
+
+    )
+}
+
+export default From
