d77acd8f33d093492a93745aaacf934867cfebf1
Emilien <emilien.ginko@gmail.com>
2023-11-18 15:11:45 +0100

    🔧 Fix astro url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "madame-guitare.fr",
+  site: "https://madame-guitare.fr",
   integrations: [react(), sitemap()],
-  output: "server",
-  adapter: vercel()
 });
